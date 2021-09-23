012a486b607459ed25b59ed33e08013f371f35af
Alex <alex.chart@turningpointhr.com>
2021-09-23 14:40:46 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+const newcats = ["Broom"];
+function appendCat () {
+    let newcats = ([...cats, "Broom"]);
+    return newcats;
+}
+const footballcat = ["Arnold"];
+function prependCat () {
+    let footballcat =(["Arnold", ...cats])
+    return footballcat;
+}
+function removeLastCat () {
+    return cats.slice(0, -1);
+}
+function removeFirstCat () {
+    const newcats = cats.slice (1);
+    return newcats;
+}
